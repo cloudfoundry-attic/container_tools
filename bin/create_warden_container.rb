f0dd386c485ba8b8ab471cf8966cf147bf157652
Phan Le and Sarah Chandler <pair+phan+schandler@pivotallabs.com>
2013-08-28 17:59:37 -0700

    Change the format a little bit. Added test for error case

--- bin/create_warden_container.rb
+++ bin/create_warden_container.rb
@@ -5,7 +5,10 @@
 #   "warden_socket_path": "/tmp/warden.sock",
 #   "bind_mounts": [
 #     {"src_path": "/tmp/foo", "dst_path": "/bar", mode: "rw"}
-#   ]
+#   ],
+#    "disk_limit": 100,
+#    "memory_limit": 200,
+#     "network": true,
 # }
 #
 # Then create a container with the specified bind mounts set up.
@@ -13,7 +16,6 @@
 # {
 #   "handle": "abc123"
 # }
-
 require "json"
 
 $:.unshift(File.expand_path("../../lib", __FILE__))
@@ -21,17 +23,25 @@ require "container/container"
 require "container/connection_provider"
 
 config = JSON.parse(STDIN.read)
-warden_socket_path = config.fetch("warden_socket_path")
-container = Container.new(ConnectionProvider.new(warden_socket_path))
-create_network = config.fetch('network')
 
-asdasd = container.create_container(
-  config.fetch("bind_mounts"),
-  config.fetch("disk_limit"),
-  config.fetch("memory_limit"),
-  create_network)
+begin
+  warden_socket_path = config.fetch("warden_socket_path")
+  container = Container.new(ConnectionProvider.new(warden_socket_path))
+  create_network = config.fetch('network')
+  container.create_container(
+    config["bind_mounts"] || [],
+    config.fetch("disk_limit"),
+    config.fetch("memory_limit"),
+    create_network)
+rescue => e
+  STDERR.puts("Failed to create a container with error:" + e.backtrace.join("\n"))
+  exit 1
+end
 
 puts({
   handle: container.handle,
-  network: container.network_ports
+  host_port: container.network_ports["host_port"],
+  container_port: container.network_ports["container_port"],
+  console_host_port: container.network_ports["console_host_port"],
+  console_container_port: container.network_ports["console_container_port"]
 }.to_json)
